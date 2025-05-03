3ac28edbd4597aefcb01cd8e50f96e08c145f679
andresdanielmtz <autummata@gmail.com>
2025-05-03 16:22:54 -0600

    Add: Australia / Head Title Click

---
+++ Object Repository/Link_Australia_Article.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Link_Australia_Article</name>
+   <tag></tag>
+   <elementGuidId>093fd4ac-afc2-4e6d-bfc3-2210ae3b2bc9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;main-content&quot;]/article/section[1]/div/div/div[1]/div[3]/div/div/div/div[2]/div[1]/a</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Scripts/ArticleLink/Script1746310514656.groovy
@@ -0,0 +1,38 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.testobject.TestObject
+
+// 1. Open browser and navigate to BBC home
+WebUI.openBrowser('')
+WebUI.maximizeWindow()
+WebUI.navigateToUrl('https://www.bbc.com')
+
+// 2. Wait for the page to load
+WebUI.waitForPageLoad(10)
+
+// 3. Locate and click the Australia article link
+TestObject australiaLink = findTestObject('Link_Australia_Article')
+WebUI.waitForElementClickable(australiaLink, 3)
+WebUI.click(australiaLink)
+WebUI.comment("Clicked the Australia article link.")
+
+
+// 6. Clean up
+WebUI.delay(2)
+WebUI.closeBrowser()
